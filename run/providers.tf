95ff40d17faaf6fc1048a8a3578d262bdb7b6b95
Kat Traxler <kat@nanooksecurity.com>
2024-03-29 14:46:45 -0500

    removed run directory

--- run/providers.tf
+++
@@ -1,6 +0,0 @@
-provider "google" {
-  region  = "us-central1"
-  project = "${var.project_id}"
-  user_project_override = true
-  billing_project = "${var.project_id}"
-}
--- run/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  description = "The ID of the project to deploy resources into"
-  type        = string
-}
-
-variable "region" {
-  description = "The geographical region to deploy resources into"
-  type        = string
-}
