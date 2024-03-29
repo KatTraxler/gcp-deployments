fe76742c97aaf5ea1f1c79e3d814bb7b0440edd3
Kat Traxler <kat@nanooksecurity.com>
2024-03-29 14:47:52 -0500

    added run directory

---
+++ run/main.tf
@@ -0,0 +1,20 @@
+########################################################################
+# Define modules
+########################################################################
+module "call_apsi" {
+    source  = "./modules/execute-api-calls"
+    project_id = var.project_id
+
+}
+
+module "call_deploy_job" {
+    source  = "./modules/cloud-run-job"
+
+
+}
+
+module "call_deploy_service" {
+    source  = "./modules/cloud-run-service"
+    
+
+}
