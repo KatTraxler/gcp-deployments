fb41118afb0afbcf916311f7ac05d00698c78a2c
Kat Traxler <ktraxler@Kats-MacBook-Pro.local>
2024-07-24 11:37:12 -0500

    updated deriviated container

--- containers/avml-deep-learning/registries.tf
+++ containers/avml-deep-learning/registries.tf
@@ -3,6 +3,6 @@ resource "google_artifact_registry_repository" "avml-deep-learning-container-rep
   project       = data.google_project.project.project_id
   location      = var.region
   repository_id = "avml-deep-learning"
-  description   = "Registry which Cloud Build can push the AVML containter to when its built"
+  description   = "Registry which Cloud Build can push the Deep Learning Workbeench + AVML containter to when its built"
   format        = "DOCKER"
 }
