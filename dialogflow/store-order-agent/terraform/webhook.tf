3ecd6c63aa8a93e993db2b2d3e28da6bc4ec8bcb
Kat Traxler <ktraxler@Kats-MacBook-Pro.local>
2025-06-30 10:24:11 -0600

    added dialogflow-agents

--- dialogflow/store-order-agent/terraform/webhook.tf
+++ dialogflow/store-order-agent/terraform/webhook.tf
@@ -4,7 +4,7 @@ resource "google_dialogflow_cx_webhook" "basic_webhook" {
   generic_web_service {
         uri = "https://example.com"
         request_headers = {
-            Authorization = "Bearer ya29.a0AW4XtxhA9yh-UPC6OOChJQvbVC5igCJPAJa_akIYyT1KRbPNVC4_oCRn71G7M322SR6rcSwC9-n3vGTLAg_dKvMDUNSfLduOTQtxpvSS89NYXZpY3FyeBSBmW9ePKj6SKVJAZsuwiz_o5t5lzjSZrOllhdM4BxHNNNQR3xisRpkYYQaCgYKAR0SARISFQHGX2MihfJprJrONbhZCEX6ijaFAA0181"
+            Authorization = "Bearer ya29."
         }
     }
     enable_stackdriver_logging = true
